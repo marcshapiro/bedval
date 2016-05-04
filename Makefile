2a054d1a007b657cbf92634ea705bc7717f91f5b
marcshapiro <marc.shapiro@gmail.com>
2016-05-03 21:44:05 -0400

    Added dependences to Makefile. Converted first pass to better functions.

--- Makefile
+++ Makefile
@@ -1,8 +1,26 @@
-run:
-	./target/debug/val
+all: run
 
-build:
+EXE := ./target/debug/val
+SRC := $(wildcard src/*.rs)
+DATA := $(wildcard sample/*.bv)
+
+run: $(EXE)
+	$(EXE)
+
+test: $(EXE)
+	cargo test
+
+build: $(EXE)
+
+$(EXE): $(SRC) Makefile
 	cargo build
+
+help:
+	echo "Targets include: run test build echo"
+
+echo:
+	echo "EXE: $(EXE)"
+	echo "SRC: $(SRC)"
 
 git1:
 	git add -n .
@@ -14,3 +32,8 @@ git2:
 
 git3:
 	git commit --dry-run
+	git commit
+
+git4:
+	git push -n
+	git push
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
+use std::io::Error;
 use std::io::prelude::*;
 use std::fs::File;
-//use std::concat!;
+use std::path::Path;
+
 fn main() {
     let filen = "sample/t1.bv";
-    let fr = File::open(filen);
-    match fr {
-        Ok(f) =>  val_file(f),
-        Err(e) => println!("{}: {}",filen, e),
-    };
+    let ok = process_file(filen);
+    match ok {
+        Ok(()) => println!("Ok"),
+        Err(e) => println!("{}: {}", filen, e),
+    }
 }
 
-fn val_file(mut f : File) -> () {
-    println!("Reading file");
+fn process_file<P: AsRef<Path>>(filename: P) -> Result<(), Error> {
+    let s = try!(read_file(filename));
+    println!("String: {}", s);
+    val_string(s);
+    Ok(())
+}
+
+fn read_file<P: AsRef<Path>>(filename: P) -> Result<String, Error> {
+    let mut fr = try!(File::open(filename));
     let mut s : &mut String = &mut String::with_capacity(29);
-    let nr = f.read_to_string(s);
-    match nr {
-        Ok(n) => println!("Read string {} {} as:{}",n,s,val_string(s.to_string())),
-        Err(e) => println!("Error: {}",e),
-    }
-
-    println!("Done file");
+    try!(fr.read_to_string(s));
+    Ok(s.to_string())
 }
 
-fn val_string(s: String) -> &'static str {
+fn val_string(s: String) {
     for cr in s.chars() {
-        println!("Char {}", cr)
+        println!("Char: {}", cr)
     }
-    "xxx"
 }
