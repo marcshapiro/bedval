37bb2f50d778412be213f7114c6438c80ead14fe
marcshapiro <marc.shapiro@gmail.com>
2016-06-04 08:45:19 -0400

    Parsed @from, @struct, @bind

--- src/ast.rs
+++ src/ast.rs
@@ -6,12 +6,12 @@ use std::fmt;
 pub enum Expr {
     Literal(String),
     Column(Vec<Expr>), // KeyColumn LCurl <Expr>* RCurl
-//    Struct(Vec<Bind>), // KeyStruct LCurl <Bind>* RCurl
+    Struct(Vec<Bind>), // KeyStruct LCurl <Bind>* RCurl
     KeyRoot,
     KeySys,
     KeyUp,
     KeyMy,
-//    From(Vec<Expr>), // KeyFrom <Expr> LCurl <Expr>_ RCurl
+    From(Vec<Expr>), // KeyFrom <Expr> LCurl <Expr>_ RCurl
     Error(String),
 }
 
@@ -20,19 +20,19 @@ impl fmt::Display for Expr {
         match *self {
             Expr::Literal(ref s) => write!(f, "lit({})", s),
             Expr::Column(ref c) => write!(f, "col({:?})", c), // FIXME: without Debug
-//            Expr::Struct(ref s) => write!(f, "str..."), // FIXME: recur
+            Expr::Struct(ref s) => write!(f, "str...({:?})",s), // FIXME: recur
             Expr::KeyRoot => write!(f, "@Root"),
             Expr::KeySys => write!(f, "@Sys"),
             Expr::KeyUp => write!(f, "@Up"),
             Expr::KeyMy => write!(f, "@My"),
-//            Expr::From(ref v) => write!(f, "from..."), // FIXME: recur
+            Expr::From(ref v) => write!(f, "from({:?})", v), // FIXME: recur / no Debug
             Expr::Error(ref s) => write!(f, "err({})", s),
         }
     }
 }
 
-/*
-#[derive(Debug)]
+
+#[derive(Debug, PartialEq)]
 pub struct Bind { // KeyBind Literal LCurl Expr RCurl
     name: String,
     value: Expr,
@@ -43,7 +43,6 @@ impl fmt::Display for Bind {
         write!(f, "Bind {} = {}", self.name, self.value)
     }
 }
-*/
 
 
 pub fn parse(toks: Vec<lex::Tok>) -> Expr {
@@ -63,22 +62,62 @@ pub fn non_gray(it: &mut vec::IntoIter<lex::Tok>) -> Option<lex::Tok> {
     }
 }
 
+fn parse_bind(it: &mut vec::IntoIter<lex::Tok>) -> Result<Bind, String> {
+    let ltok = non_gray(it);
+    match ltok {
+        Some(lex::Tok::Literal(name)) =>
+            match non_gray(it) {
+                Some(lex::Tok::CurlL) => {
+                    let expr = parse_expr(None, it);
+                    match non_gray(it) {
+                        Some(lex::Tok::CurlR) => Ok(Bind{name:name, value:expr}),
+                        _ => Err("@bind must end with '}'".to_string()),
+                    }
+                },
+                _ => Err("@bind <name> must be followed by '{'".to_string()),
+            },
+        _ => Err("@bind must be followed by literal".to_string()),
+    }
+}
+
+fn parse_binds(it: &mut vec::IntoIter<lex::Tok>) -> Result<(Vec<Bind>, Option<lex::Tok>), String> {
+    let mut binds = vec![];
+    let mut next_tok = non_gray(it);
+    while let Some(lex::Tok::Key(lex::Key::Bind)) = next_tok {
+        // FIXME: try!
+        let rbind = parse_bind(it);
+        match rbind {
+            Ok(bind) => binds.push(bind),
+            Err(s) => return Err(s)
+        }
+        next_tok = non_gray(it);
+    }
+    Ok((binds, next_tok))
+}
+
 fn parse_expr(ofirst_tok: Option<lex::Tok>, it: &mut vec::IntoIter<lex::Tok>) -> Expr {
     let first_tok = match ofirst_tok {
         Some(ft) => ft,
-        None => match it.next() {
+        None => match non_gray(it) {
             Some(ft) => ft,
             None => return Expr::Error("Expected Expr, got EOF".to_string())
         }
     };
     match first_tok {
         lex::Tok::Key(lex::Key::Struct) => {
-            // ...
-            Expr::Error("Struct NYI".to_string())
-        },
-        lex::Tok::Key(lex::Key::From) => {
-            // ...
-            Expr::Error("From NYI".to_string())
+            match non_gray(it) {
+                Some(lex::Tok::CurlL) => {
+                    match parse_binds(it) {
+                        Ok((binds, end_tok)) =>
+                            match end_tok {
+                                Some(lex::Tok::CurlR) => Expr::Struct(binds),
+                                _ => Expr::Error("@struct must end with '}'".to_string()),
+                            },
+                        Err(e) => Expr::Error(e),
+                    }
+                },
+                _ => Expr::Error("@struct must be followed by '{'".to_string()),
+            }
         },
         lex::Tok::Key(lex::Key::Column) => {
             let otok = non_gray(it);
@@ -97,6 +136,26 @@ fn parse_expr(ofirst_tok: Option<lex::Tok>, it: &mut vec::IntoIter<lex::Tok>) ->
         lex::Tok::Key(lex::Key::Up) => Expr::KeyUp,
         lex::Tok::Key(lex::Key::Sys) => Expr::KeySys,
         lex::Tok::Key(lex::Key::My) => Expr::KeyMy,
+        lex::Tok::Key(lex::Key::From) => {
+            let otok1 = non_gray(it);
+            let head_expr = parse_expr(otok1, it);
+            let otok2 = non_gray(it);
+            match otok2 {
+                Some(lex::Tok::CurlL) => {
+                    let (tail_exprs, ttok) = parse_exprs(it);
+                    match ttok {
+                        Some(lex::Tok::CurlR) => {
+                            let mut all_exprs = Vec::with_capacity(1+tail_exprs.len());
+                            all_exprs.push(head_expr);
+                            all_exprs.extend(tail_exprs);
+                            Expr::From(all_exprs)
+                        },
+                        _ => Expr::Error("@from must end with '}'".to_string()),
+                    }
+                },
+                _ => Expr::Error("@from must have '{' after root struct".to_string()),
+            }
+        },
         lex::Tok::Literal(s) => Expr::Literal(s),
         _ => Expr::Error("Unexpected token".to_string())
     }
@@ -114,11 +173,11 @@ fn parse_exprs(it: &mut vec::IntoIter<lex::Tok>) -> (Vec<Expr>, Option<lex::Tok>
                     | lex::Tok::Key(lex::Key::Column)
                     | lex::Tok::Key(lex::Key::My)
                     | lex::Tok::Literal(_) => {
-                        println!("parse_exprs literal/key: {:?}", t);
+                        //println!("parse_exprs literal/key: {:?}", t);
                         exprs.push(parse_expr(Some(t), it));
                     },
                     _ => {
-                        println!("parse_exprs other: {:?}", t);
+                        //println!("parse_exprs other: {:?}", t);
                         return (exprs, Some(t))
                     },
                 }
@@ -138,6 +197,14 @@ fn assert_no_lex_errors(toks: &Vec<lex::Tok>) {
             lex::Tok::Error(_) => assert!(false),
             _ => {}
         }
+    }
+}
+
+#[cfg(test)]
+fn is_error(expr: &Expr) -> bool {
+    match expr {
+        &Expr::Error(_) => true,
+        _ => false,
     }
 }
 
@@ -167,4 +234,49 @@ fn test_column_empty() {
 fn test_column_some() {
     assert_eq!(sparse("@column { @my @my @my }"),
         Expr::Column(vec![Expr::KeyMy, Expr::KeyMy, Expr::KeyMy]));
+}
+
+#[test]
+fn test_from_end() {
+    let a = sparse("@from");
+    assert!(is_error(&a));
+}
+
+#[test]
+fn test_from_no_root() {
+    let a = sparse("@from { @my }");
+    assert!(is_error(&a));
+}
+
+#[test]
+fn test_from_empty() {
+    assert_eq!(sparse("@from @my { }"), Expr::From(vec![Expr::KeyMy]));
+}
+
+#[test]
+fn test_from() {
+    assert_eq!(sparse("@from @my { @my @my }"),
+        Expr::From(vec![Expr::KeyMy, Expr::KeyMy, Expr::KeyMy]));
+}
+
+#[test]
+fn test_struct_empty() {
+    assert_eq!(sparse("@struct {}"), Expr::Struct(vec![]));
+}
+
+#[test]
+fn test_struct_one() {
+    assert_eq!(sparse("@struct { @bind a { @my }}"),
+        Expr::Struct(vec![Bind{name:"a".to_string(), value: Expr::KeyMy}]))
+}
+
+#[test]
+fn test_struct_two() {
+    assert_eq!(
+        sparse("@struct { @bind abc { @my } @bind bcd { @root }}"),
+        Expr::Struct(vec![
+                Bind{name:"abc".to_string(), value: Expr::KeyMy},
+                Bind{name:"bcd".to_string(), value: Expr::KeyRoot},
+            ])
+        )
 }
